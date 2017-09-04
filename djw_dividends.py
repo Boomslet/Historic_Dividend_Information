0545fb7c0c97dc00870ea947288b123d159d8a59
Mark Boon <Boomslet@users.noreply.github.com>
2017-09-05 01:00:28 +0800

    Update djw_dividends.py

--- djw_dividends.py
+++ djw_dividends.py
@@ -13,8 +13,8 @@ and the BeautifulSoup library:
 https://www.crummy.com/software/BeautifulSoup/
 
 Author: Mark Boon
-Date: 7/08/2017
-Version: 1.1
+Date: 05/09/2017
+Version: 1.1.1
 """
 
 import urllib.request
