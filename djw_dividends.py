b0d2c9fc6986a1f7f0270ba0007de3ddb7a31cb5
Mark Boon <Boomslet@users.noreply.github.com>
2017-12-21 10:40:33 +0800

    Add files via upload

--- djw_dividends.py
+++ djw_dividends.py
@@ -1,3 +1,5 @@
+import urllib.request
+
 """
 A script that retrieves historic data regarding dividends
 paid by Djerriwarrh Investments to shareholders
@@ -13,31 +15,35 @@ and the BeautifulSoup library:
 https://www.crummy.com/software/BeautifulSoup/
 
 Author: Mark Boon
-Date: 05/09/2017
-Version: 1.1.1
+Date: 21/12/2017
+Version: 2.0
 """
 
-import urllib.request
 from bs4 import BeautifulSoup
 
 
-units_held = 0
-url = "http://www.djerri.com.au/Dividend-History.aspx"
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, "html.parser")  # default html parser
-data = soup.find_all("td")
+def djw_summary(units=0):
 
-print("DJW Dividend Summary: \n")
-print('{:14}'  '{:5}'  '{:9}'  '{:}'
-      .format("Date", "CPS", "Franked", "Ex-Yield"))
+    url = "http://www.djerri.com.au/Dividend-History.aspx"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")  # default html parser
+    data = soup.find_all("td")
 
-for i in range(8, len(data) - 4, 4):  # iterating by 4 allows i to correctly index every row of information
+    print("DJW Dividend Summary: \n")
+    print('{:14}'  '{:5}'  '{:9}'  '{:}'
+          .format("Date", "CPS", "Franked", "Ex-Yield"))
 
-    date = data[i].text.strip().split(" ")
-    date = date[0] + "/" + date[1][:3] + "/" + date[-1]
-    cps = float(data[i + 1].text.strip())
-    franked = str(data[i + 2].text.strip() == "100.00%")
-    ex_yield = (cps / 100) * units_held
+    for i in range(8, len(data) - 4, 4):
 
-    print('{:14}'  '{:<5.0f}'  '{:10}'  '{:}'  '{:.2f}'
-          .format(date, cps, franked, "$", ex_yield))
+        date = data[i].text.strip().split(" ")
+
+        date = date[0] + "/" + date[1][:3] + "/" + date[-1]
+
+        cps = float(data[i + 1].text.strip())
+
+        franked = str(data[i + 2].text.strip() == "100.00%")
+
+        ex_yield = (cps / 100) * units
+
+        print('{:14}'  '{:<5.0f}'  '{:10}'  '{:}'  '{:.2f}'
+              .format(date, cps, franked, "$", ex_yield))
